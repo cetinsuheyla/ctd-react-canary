6584361757a714aa7404633f2b2045756cda849b
Suheyla Cetin <Suheyla@mehmets-MacBook-Pro.local>
2021-12-08 11:26:13 -0800

    Add remove button to remove specific todo

--- src/InputWithLabel.js
+++ src/InputWithLabel.js
@@ -1,21 +1,22 @@
-import React, {useRef, useEffect} from 'react'
+import React, { useRef, useEffect } from "react";
 
-function InputWithLabel({todoTitle, handleTitleChange, children}) {
-  const inputRef = useRef();
-  useEffect(()=> {inputRef.current.focus()})
-  return (
-    <div>
-      <label htmlFor="todoTitle">{children}</label>
-				<input
-          ref={inputRef}
-					value={todoTitle}
-					onChange={handleTitleChange}
-					id="todoTitle"
-					name="title"
-				></input>
-    </div>
-  )
+function InputWithLabel({ todoTitle, handleTitleChange, children }) {
+	const inputRef = useRef();
+	useEffect(() => {
+		inputRef.current.focus();
+	});
+	return (
+		<div>
+			<label htmlFor="todoTitle">{children}</label>
+			<input
+				ref={inputRef}
+				value={todoTitle}
+				onChange={handleTitleChange}
+				id="todoTitle"
+				name="title"
+			></input>
+		</div>
+	);
 }
 
-export default InputWithLabel
-
+export default InputWithLabel;
